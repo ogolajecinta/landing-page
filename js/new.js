fffceb064b54e6729517c856d98e2dbcb3d19d8c
unknown <jessieatieno98@gmail.com>
2022-07-19 13:00:13 +0300

    Update files

---
+++ js/new.js
@@ -0,0 +1,8 @@
+//Express server on port 3000
+var express = require('express');
+//selenium web-driver
+var webdriver = require('selenium-webdriver');
+//return time currently
+var moment = require('moment');
+//return current time
+var time = moment().format('MMMM Do YYYY, h:mm:ss a');
